2983daab96644eb6c7bb0921e94402612e61cea0
rf7 <rf7@rice.edu>
2014-02-07 11:36:42 -0600

    added post method

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,9 +1,25 @@
 var sharedTextApp = angular.module('sharedTextApp', []);
 
 sharedTextApp.controller('SharedTxtCtrl', function($scope, $http) {
+	
+	// Event Listeners
+	if (!!window.EventSource) {
+		var source = new EventSource('data/receive');
+	}
 
+	source.addEventListener('message', function(e) {
+		$scope.receivedText = e.data;
+	}, false);
+
+	source.addEventListener('open', function(e) {
+	}, false);
+
+	source.addEventListener('error', function(e) {
+	}, false);
+
+	// scopes
     $scope.sendData = function() {
-        // ... do $http.post
+        $http.post('data/receive', $scope.inputText);
     };
 
 });
