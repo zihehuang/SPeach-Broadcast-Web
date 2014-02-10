2bc38e2cba7549289df17ffc1c654f079438a267
rf7 <rf7@rice.edu>
2014-02-10 11:27:28 -0600

    this commit is broken. testing whether api/stream and api/add works

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -4,10 +4,10 @@ sharedTextApp.run(function(editableOptions) {
   editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
 });
 
-sharedTextApp.controller('SharedTxtCtrl', function($scope, $http) {
+sharedTextApp.controller('SharedTxtCtrl', function($scope, $http, $filter) {
 	
 	// Event Listeners
-    var source = new EventSource('data/get');
+    var source = new EventSource('api/stream');
 
 	source.addEventListener('message', function(e) {
 	    $scope.$apply(function() {
@@ -23,20 +23,47 @@ sharedTextApp.controller('SharedTxtCtrl', function($scope, $http) {
 	}, false);
 
 	// scopes
-    $scope.sendData = function(data) {
+    $scope.sendData = function() {
         $http({
             method: 'POST',
             headers: {
                 'Content-Type': 'text/plain',
             },
-            url: 'data/receive',
+            url: 'api/add',
+            data: $scope.inputText
+        });
+
+        $scope.inputText = ""
+    };
+
+    $scope.sendDataFromEditables = function(data) {
+        $http({
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/plain',
+            },
+            url: 'api/receive',
             data: data
         });
     };
 
     $scope.editables = [
-    	{name: 'Edit Me'},
-    	{name: 'Edit Me too'}
+    	{name: 'Edit Me'}
     ];
 
+    $scope.user = {
+        status: 2
+    }; 
+
+    $scope.statuses = [
+        {value: 1, text: 'status1'},
+        {value: 2, text: 'status2'},
+        {value: 3, text: 'status3'},
+        {value: 4, text: 'status4'}
+    ]; 
+
+    $scope.showStatus = function() {
+        var selected = $filter('filter')($scope.statuses, {value: $scope.user.status});
+        return ($scope.user.status && selected.length) ? selected[0].text : 'Not set';
+    };
 });
